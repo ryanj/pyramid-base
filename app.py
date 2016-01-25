f658915ceb231a804ea173c9954d9c8deeec8faf
ryanj <ryan.jarvinen@gmail.com>
2016-01-25 14:37:27 -0800

    openshift V2 support

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import os
 
 def hello(request):
     return Response('Hello world!')
 
 if __name__ == '__main__':
+    port = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
+    ip = os.environ.get('OPENSHIFT_PYTHON_IP','0.0.0.0')
     config = Configurator()
     config.add_route('hello_world', '/')
     config.add_view(hello, route_name='hello_world')
