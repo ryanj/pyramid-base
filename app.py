2ec0394f587a4b3677aacee366020406ac968b80
ryanj <ryan.jarvinen@gmail.com>
2016-01-26 23:33:02 -0800

    adding a static folder and a basic stylesheet

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
-def hello(request):
-    return Response('Hello world!')
+def home(request):
+	return Response("<head><link href='/static/css/site.css' rel='stylesheet' type='text/css' /></head><body><h1>Hello from Pyramid!</h1><p>Learn how to extend this example at <a href='http://trypyramid.com/'>TryPyramid.com</a>.</body>")
 
 if __name__ == '__main__':
     port = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
     ip = os.environ.get('OPENSHIFT_PYTHON_IP','0.0.0.0')
     config = Configurator()
-    config.add_route('hello_world', '/')
-    config.add_view(hello, route_name='hello_world')
+    #config.include('pyramid_jinja2')
+    config.add_route('home', '/')
+    config.add_view(home, route_name='home')
+    config.add_static_view(name='static', path='static')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
